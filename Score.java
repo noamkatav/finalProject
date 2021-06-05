59f970af7c93a26d3bb4d4d9b05b2b7e0b7b9307
noamkatav <77569278+noamkatav@users.noreply.github.com>
2021-06-05 23:08:02 +0300

    Add files via upload

---
+++ Score.java
@@ -0,0 +1,20 @@
+package finalProject;
+
+public class Score {
+	public int playerScore;
+	public int computerScore;
+	
+	
+public Score()//constructor
+{
+	playerScore = 0;
+	computerScore =0;
+}
+
+public Score(int player,int computer)//constructor
+{
+	this.playerScore = player;
+	this.computerScore = computer;
+}
+
+}
